4182cf43b6154656bc1ab6d6a7fb55be9e05654d
Brian <briguy22811@gmail.com>
2018-02-21 22:27:55 -0500

    Template Spring app with line controller, line class and Application class

---
+++ src/main/java/hello/Line.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class Line {
+
+    private final String content;
+
+    public Line(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/hello/LineController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LineController {
+
+    private static final String template = "Serving up line number %s";
+
+    @RequestMapping("/line")
+    public Line line(@RequestParam(value="line", defaultValue="0") String line) {
+        return new Line(String.format(template, line));
+    }
+}
