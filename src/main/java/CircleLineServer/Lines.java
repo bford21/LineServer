8272b0f918599e1910ffed7f8cb096a84968e2ce
Brian <briguy22811@gmail.com>
2018-02-25 14:01:52 -0500

    Removed references to gradle. Using Maven instead

--- src/main/java/CircleLineServer/Lines.java
+++ src/main/java/CircleLineServer/Lines.java
@@ -6,7 +6,7 @@ public class Lines {
     private static HashMap<Integer, String> allLines = new HashMap<>();
 
     public static String getLine(Integer lineNum){
-      return allLines.get(lineNum);
+        return allLines.get(lineNum);
     }
 
     public static void insertLine(Integer lineNum, String line){
