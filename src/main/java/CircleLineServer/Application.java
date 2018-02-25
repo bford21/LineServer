e4057082fb4d76937d396e2be9c4d43856ea126c
Brian <briguy22811@gmail.com>
2018-02-25 13:21:38 -0500

    Refactor class to use new Lines class

--- src/main/java/CircleLineServer/Application.java
+++ src/main/java/CircleLineServer/Application.java
@@ -5,13 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Scanner;
 
 @SpringBootApplication
 public class Application {
-    public static HashMap<Integer, String> map = new HashMap<>();
-
     public static void main(String[] args) throws IOException {
         FileInputStream inputStream = null;
         Scanner sc = null;
@@ -23,28 +20,24 @@ public class Application {
             // Specified UTF-8 encoding to support all valid ASCII characters
             sc = new Scanner(inputStream, "UTF-8");
 
+            System.out.print("Processing file...");
+
+            // Insert each line into a hashMap with <key>=line number & <value>=text on line
             int i =1;
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
-                // Insert each line into a hashMap with <key>=line number & <value>=text on line
-                map.put(i,line);
+                Lines.insertLine(i, line);
                 i++;
             }
 
             // Scanner suppresses exceptions so check for ioException
-            if (sc.ioException() != null) {
-                throw sc.ioException();
-            }
+            if (sc.ioException() != null) throw sc.ioException();
         } catch (Exception e) {
             e.printStackTrace();
         }  finally {
             // Close inputStream and scanner
-            if (inputStream != null) {
-                inputStream.close();
-            }
-            if (sc != null) {
-                sc.close();
-            }
+            if (inputStream != null) inputStream.close();
+            if (sc != null) sc.close();
         }
 
         // Launch application
